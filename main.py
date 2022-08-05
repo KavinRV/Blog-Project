ca405f69ca785df4098dbdd30affa4146edd7a27
Kavin R V <59758578+KavinRV@users.noreply.github.com>
2022-08-05 15:43:45 +0530

    Give more power to admin

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def inject_now():
 def admin_only(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if current_user.id != 1 and current_user.id != 2:
+        if current_user.id != 1:
             return abort(403)
         return f(*args, **kwargs)
     return decorated_function
